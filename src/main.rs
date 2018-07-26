0f3e6084f94bb80cb2d5edec51bfb3f5cb8e13ff
xinbg <biguo_xin@pinssible.com>
2018-07-26 17:33:03 +0800

    use global mongo db connection

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,9 @@ use mongodb::{Client, ThreadedClient};
 use rocket::response::content;
 use rocket::response::NamedFile;
 
+// Global mongodb client
+static mut MC: Option<Client> = None;
+
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
@@ -30,39 +33,42 @@ fn index() -> io::Result<NamedFile> {
 
 #[get("/blog_posts")]
 fn get_blog_posts() -> content::Json<String> {
-    // TODO: make client globle
-    let client =
-        Client::connect("localhost", 27017).expect("Failed to initialize standalone client.");
+    unsafe {
+        match MC {
+            Some(ref client) => {
+                let coll = client.db("blog").collection("posts");
 
-    let coll = client.db("blog").collection("posts");
+                let doc = doc! {
+                    "title": "Hello Github",
+                    "body":"Golang 相关推荐项目",
+                    "url":"https://gist.github.com/FrontMage/6b54b9711cdf4656c42c4973b09b7167",
+                };
 
-    let doc = doc! {
-        "title": "Hello Github",
-        "body":"Golang 相关推荐项目",
-        "url":"https://gist.github.com/FrontMage/6b54b9711cdf4656c42c4973b09b7167",
-    };
+                // Find the document and receive a cursor
+                // TODO: error handle
+                let cursor = coll.find(Some(doc.clone()), None)
+                    .ok()
+                    .expect("Failed to execute find.");
 
-    // Find the document and receive a cursor
-    // TODO: error handle
-    let cursor = coll.find(Some(doc.clone()), None)
-        .ok()
-        .expect("Failed to execute find.");
+                let mut contents = Vec::new();
 
-    let mut contents = Vec::new();
+                for item in cursor {
+                    match item {
+                        Ok(doc) => if let Ok(j) = serde_json::ser::to_string(&doc) {
+                            contents.push(j);
+                        },
+                        Err(e) => {
+                            println!("{:?}", e);
+                        }
+                    }
+                }
 
-    for item in cursor {
-        match item {
-            Ok(doc) => if let Ok(j) = serde_json::ser::to_string(&doc) {
-                contents.push(j);
-            },
-            Err(e) => {
-                println!("{:?}", e);
+                // TODO: stripe the _id field
+                content::Json(format!("[{}]", contents.join(",")))
             }
+            None => content::Json(String::from("[]")),
         }
     }
-
-    // TODO: stripe the _id field
-    content::Json(format!("[{}]", contents.join(",")))
 }
 
 #[get("/<file..>")]
@@ -77,5 +83,10 @@ fn rocket() -> rocket::Rocket {
 }
 
 fn main() {
+    unsafe {
+        let client =
+            Client::connect("localhost", 27017).expect("Failed to initialize standalone client.");
+        MC = Some(client);
+    }
     rocket().launch();
 }
